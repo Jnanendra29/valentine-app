7e7a4e3f94389c6b751c62f3248790e876985a8b
Jnanendra29 <jnanendrasenapati29@gmail.com>
2024-02-29 08:00:12 +0530

    project commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  }
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be like this!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
+
+
+
+  return (
+    <div className="centered-container">
+      <div className="valentine-container">
+        {yesPressed ? (
+          <>
+            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="kisses" />
+            <div className="text-conatiner">Ok yay!!!.. I Love Youuuu</div>
+          </>
+        ) : (
+          <>
+            <img
+              className="h-[200px]"
+              style={{ width: "400x", height: "240px" }}
+              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif" alt="holding-roses"
+            />
+            <h1 className="text-container">Will you be my Valentine?</h1>
+            <div>
+              <button className={"yes-button"}
+                style={{ fontSize: yesButtonSize }}
+                onClick={() => setYesPressed(true)}>
+                Yes
+              </button>
+
+              <button onClick={handleNoClick} className="no-button">
+                {noCount === 0 ? "No" : getNoButtonText()}
+              </button>
+            </div>
+          </>
+        )
+        }
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
